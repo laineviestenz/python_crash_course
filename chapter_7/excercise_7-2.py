2f898aac702b7db86cf8deeb390edbe81cf92f0d
Laine Viestenz <laineviestenz@gmail.com>
2025-12-18 21:21:55 -0600

    finished chapter 7

---
+++ chapter_7/excercise_7-2.py
@@ -0,0 +1,6 @@
+num_seats = int(input("How many people are in your party? "))
+
+if num_seats > 8:
+    print("One minute, you will have to wait for a table.")
+else:
+    print('Good news, I can seat you right away')
