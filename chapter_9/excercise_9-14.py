85649075477a766c420da9ab691a96e0c6d4f2e3
Laine Viestenz <laineviestenz@gmail.com>
2026-01-09 11:19:16 -0600

    added docstrings to 9-14

--- chapter_9/excercise_9-14.py
+++ chapter_9/excercise_9-14.py
@@ -1,14 +1,21 @@
 from random import randint
+
 class Die():
+    """creates a die with a default value of six sides, but can be updated to 
+    any size"""
+    
     def __init__(self, sides = 6):
         self.sides = sides
 
     def roll_dice(self):
+        """chooses a random integer between 1 and the number of sides defined."""
         x = randint(1, self.sides)
         print(x)
 
+#default 6 sided die
 regular = Die()
 regular.roll_dice()
 
+#10 sided die
 deca = Die(10)
 deca.roll_dice()
