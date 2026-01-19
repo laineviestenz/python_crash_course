c8d98e369e06a193efd3783b98121bbe5166312a
Laine Viestenz <laineviestenz@gmail.com>
2026-01-19 11:36:11 -0600

    moved 10-6 to 10-7 and simplified 10-6 as describe test

--- chapter_10/excercise_10-6.py
+++ chapter_10/excercise_10-6.py
@@ -1,17 +1,11 @@
 print("Enter two numbers to add together: ")
-print("Enter 'q' to end.")
 
-while True:
-    a = input("Enter the first number: ")
-    if a == 'q':
-        break
-    b = input('Enter the second number: ')
-    if b == 'q':
-        break
+a = input("Enter the first number: ")
+b = input('Enter the second number: ')
 
-    try:
-        answer = int(a) + int(b)
-    except ValueError:
-        print("please provide a valid input")
-    else:
-        print(answer)
+try:
+    answer = int(a) + int(b)
+except ValueError:
+    print("please provide a valid input")
+else:
+    print(answer)
