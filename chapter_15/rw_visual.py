01c387c9a5b8f0913074572e29ff81ecde50ee99
Laine Viestenz <laineviestenz@gmail.com>
2026-01-27 12:58:26 -0600

    fixed file name and added color map

--- chapter_15/rw_visual.py
+++ chapter_15/rw_visual.py
@@ -9,7 +9,8 @@ rw.fill_walk()
 #plot the points in the walk
 plt.style.use('classic')
 fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+point_numbers = range(rw.num_points)
+ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
 ax.set_aspect('equal')
 
 plt.show()
