d19ffe493bdf4ba6ee68f6ac268074d9778c995a
Laine Viestenz <laineviestenz@gmail.com>
2026-01-27 13:15:06 -0600

    worked on 15-3 and making modifications

--- chapter_15/excercise_15-3.py
+++ chapter_15/excercise_15-3.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 
-from random_walk import RandomWalk
+from chapter_15.rw_modified import RandomWalk
 
 #make a random walk
-rw = RandomWalk(50000)
+rw = RandomWalk(500)
 rw.fill_walk()
 
 #plot the points in the walk
 plt.style.use('classic')
 fig, ax = plt.subplots(figsize=(15,9))
 point_numbers = range(rw.num_points)
-ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
+ax.plot(rw.x_values, rw.y_values, linewidth=1)
 ax.set_aspect('equal')
 #the points could also be set to 0,0 since that is always the starting point, this
 #way just offers backup incase we change the rw funciton
 ax.scatter(rw.x_values[0], rw.y_values[0], c='green', edgecolors='none', s=100)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 plt.show()
+
