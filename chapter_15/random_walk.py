9576213df60e5565616d8d69a708418256043268
Laine Viestenz <laineviestenz@gmail.com>
2026-01-31 20:59:58 -0600

    added methods to modify the step options

--- chapter_15/random_walk.py
+++ chapter_15/random_walk.py
@@ -7,24 +7,41 @@ class RandomWalk:
     def __init__(self, num_points = 5000):
         """initialize attributes of a walk."""
         self.num_points = num_points
-
         #all walks start at (0,0)
         self.x_values = [0]
         self.y_values = [0]
+        #define and set default direction values
+        self.xdirection = [1, -1]
+        self.ydirection = [1, -1]
+        #define and set default distance values
+        self.xdistance = [0,1,2,3,4]
+        self.ydistance = [0,1,2,3,4]
+
+    def set_x_direction(self, xdirection):
+        self.xdirection = xdirection
+
+    def set_y_direction(self, ydirection):
+        self.ydirection = ydirection
+
+    def set_x_distance(self, xdistance):
+        self.xdistance = xdistance
+
+    def set_y_distance(self, ydistance):
+        self.ydistance = ydistance
 
     def fill_walk(self):
         """calculate all the points in the walk"""
-        
+
         #keep taking steps until the walk reaches the desired length
         while len(self.x_values) < self.num_points:
             
             #decide which direction to go, and how far to go
-            x_direction = choice([1, -1])
-            x_distance = choice([0,1,2,3,4])
+            x_direction = choice(self.xdirection)
+            x_distance = choice(self.xdistance)
             x_step = x_direction * x_distance
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0,1,2,3,4])
+            y_direction = choice(self.ydirection)
+            y_distance = choice(self.ydistance)
             y_step = y_direction * y_distance
 
             #reject moves that go no where
